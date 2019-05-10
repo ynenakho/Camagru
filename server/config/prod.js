ea302cb0955775e4e747603d53b7ccb131bbe9f0
Yuriy Nenakhov <yuriy.nenakhov@gmail.com>
2019-05-09 20:46:04 -0700

    fixed clientURI in prod keys

--- server/config/prod.js
+++ server/config/prod.js
@@ -3,5 +3,5 @@ module.exports = {
   sendGridId: process.env.SEND_GRID_ID,
   sendGridKey: process.env.SEND_GRID_KEY,
   jwtTokenSecret: process.env.JWT_TOKEN_SECRET,
-  clientURI: CLIENT_URI
+  clientURI: process.env.CLIENT_URI
 };
